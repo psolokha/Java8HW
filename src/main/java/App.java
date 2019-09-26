0f38bfcb505507893ef9297450b448486d5c4a7c
Pavel Solokha <pavel.solokha@gmail.com>
2019-09-26 15:15:37 +0300

    version 1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,45 +1,53 @@
 import com.google.gson.*;
 
 import java.io.*;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class App {
 
-    public static void main(String... args){
-
-        Consumer<String> printer = System.out::println;
-        orgList.forEach((o) -> printer.accept(o.toString()));
-
-        printPastDueSecuritites();
-
-        printByDate("31/01/01");
-
-        printCurrencies("USD");
-
-
-
-
-
-    }
-
     public static ArrayList<Organization> orgList = new ArrayList<>();
+    public static LocalDate currentDate = LocalDate.now();
+    public static Consumer<String> printer = System.out::println;
+    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     static {
-        try (FileReader reader = new FileReader("/home/pavelsolokha/dev/java8hw/src/main/resources/test.json")) {
-            JsonArray jsonArray = new JsonParser().parse(reader).getAsJsonArray();
+        printer.accept("Введите путь к файлу: ");
+        try  {
+            FileReader fileReader = new FileReader(reader.readLine());
+            JsonArray jsonArray = new JsonParser().parse(fileReader).getAsJsonArray();
 
             Gson gson = new Gson();
             for (JsonElement element : jsonArray) {
                 orgList.add(gson.fromJson(element, Organization.class));
             }
+            printer.accept("Создан список организайи\n");
         } catch (IOException ex) {
-            System.out.println("Не получается открыть файл");
+            printer.accept("Не получается открыть файл\nРабота программы приостановлена.\nПроверьте правильность пути и запустите программу повторно.");
             ex.printStackTrace();
         }
     }
 
+    public static void main(String... args) {
+
+        printer.accept("Список организаций: ");
+
+        if (!(orgList.isEmpty()))orgList.forEach((o) -> printer.accept(o.toString()));
+        printPastDueSecuritites();
+        printByDate();
+        printCurrencies();
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     public static ArrayList<Integer> getDate(String date) {
         StringTokenizer tokenizer = new StringTokenizer(date, "/-:.,\\");
@@ -56,40 +64,64 @@ public class App {
         return dateList;
     }
 
-    public static void printCurrencies(String currency) {
-        orgList.forEach(organization -> {
-            Securities[] sec = organization.getSecurities();
+    public static void printCurrencies() {
+        if (orgList.isEmpty()) return;
+        printer.accept("Введите код валюты: ");
+        try {
+            String currency = reader.readLine();
+
+        printer.accept(orgList.stream().filter(o -> {
+            Securities[] sec = o.getSecurities();
             for (Securities s: sec) {
-                if (s.getCurrency().getCode().equals(currency.toUpperCase())) System.out.println(s.getId() + " - " + s.getCode());
-            }
-        });
-    }
-
-    public static void printByDate(String date) {
-
-        LocalDate targetDate = LocalDate.of(getDate(date).get(2), getDate(date).get(1), getDate(date).get(0));
-
-        orgList.stream().filter(organization -> {
-            LocalDate orgDate = LocalDate.of(getDate(organization.getEgrul_date()).get(0), getDate(organization.getEgrul_date()).get(1), getDate(organization.getEgrul_date()).get(2));
-            return orgDate.compareTo(targetDate) > 0? true:false;
-        }).forEach(organization -> System.out.println(organization.toString()));
-
-    }
-
-
-    public static void printPastDueSecuritites() {
-
-        LocalDate currentDate = LocalDate.now();
-        System.out.println("Количество просроченных бумаг: " + orgList.stream().filter((org) -> {
-            Securities[] sec = org.getSecurities();
-            for (Securities s: sec) {
-                LocalDate dateExpired = LocalDate.of(getDate(s.getDate_to()).get(0), getDate(s.getDate_to()).get(1), getDate(s.getDate_to()).get(2));
-                if (dateExpired.compareTo(currentDate) < 0) {
-                    System.out.println(s.getCode() + " - " + s.getDate_to() + " - " + s.getName_full());
+                if (s.getCurrency().getCode().equals(currency.toUpperCase())) {
+                    printer.accept(s.getId() + " - " + s.getCode());
                     return true;
                 }
             }
             return false;
-        }).count());
+            }).count()==0?"Валюты с таким кодом не найдено" : "Готово");
+        } catch (IOException e) {
+            System.out.println("Введено неверное значение");
+            e.printStackTrace();
+        }
     }
+
+    public static void printByDate() {
+
+        if (orgList.isEmpty()) return;
+
+        printer.accept("Введите дату для отображения организай\nсозданных после этой даты: ");
+        try  {
+            String date = reader.readLine();
+
+            LocalDate targetDate = LocalDate.of(getDate(date).get(2), getDate(date).get(1), getDate(date).get(0));
+            ArrayList<Organization> tmpList = orgList.stream().filter(organization -> {
+                LocalDate orgDate = LocalDate.of(getDate(organization.getEgrul_date()).get(0), getDate(organization.getEgrul_date()).get(1), getDate(organization.getEgrul_date()).get(2));
+                return orgDate.compareTo(targetDate) > 0 ? true : false;
+            }).collect(Collectors.toCollection(ArrayList::new));
+
+            if (tmpList.isEmpty()) printer.accept("Нет организаций зарегистрированных после этой даты.\n");
+            else tmpList.forEach(o -> printer.accept(o.toString()));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    public static void printPastDueSecuritites() {
+        if (orgList.isEmpty()) return;
+        printer.accept("Количество просроченных бумаг: " + orgList.stream().filter((org) -> {
+            Securities[] sec = org.getSecurities();
+            for (Securities s: sec) {
+                LocalDate dateExpired = LocalDate.of(getDate(s.getDate_to()).get(0), getDate(s.getDate_to()).get(1), getDate(s.getDate_to()).get(2));
+                if (dateExpired.compareTo(currentDate) < 0) {
+                    printer.accept(s.getCode() + " - " + s.getDate_to() + " - " + s.getName_full());
+                    return true;
+                }
+            }
+            return false;
+        }).count() + "\n");
+    }
+
 }
